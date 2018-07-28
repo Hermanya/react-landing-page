c1d1ed469e4c3f56c931284156c8062343408634
Herman Starikov <hermanstarikov@gmail.com>
2018-07-28 16:49:08 -0400

    add .nojekyll

--- docs/_template.js
+++ docs/_template.js
@@ -10,7 +10,7 @@ module.exports = ({
 `<!DOCTYPE html>
 <head>
 <meta charset='utf-8'>
-<meta name='viewport' content='width=device-width,initial-scale=1'>
+<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
 <meta name='generator' content='Compositor x0'>
 <title>Landing Page with React</title>
 ${meta.map(({ name, content }) => `<meta name='${name}' content='${content}'>`).join('\n')}
