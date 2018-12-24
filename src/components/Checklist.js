203b6b3b992a92113fb607937c450921c6786841
Herman Starikov <hermanstarikov@gmail.com>
2018-12-24 15:55:41 -0500

    fix checklist typing

--- src/components/Checklist.js
+++ src/components/Checklist.js
@@ -6,19 +6,19 @@ const Item = ({
   text,
   last
 }) => <Flex is='li' mt={[2]} mb={2} pb={3} alignItems='center' justifyContent='space-between'
-  style={{borderBottom: last ? 'none' : '1px solid rgba(128, 128, 128, 0.5)'}}>
+  style={{ borderBottom: last ? 'none' : '1px solid rgba(128, 128, 128, 0.5)' }}>
   <Lead>{text}</Lead><Box ml={[2, 5]} fontSize={3}>{checkmark}</Box>
 </Flex>
 
 Item.propTypes = {
-  checkmark: propTypes.string,
+  checkmark: propTypes.any,
   text: propTypes.string,
   last: propTypes.bool
 }
 
-const Checklist = ({checkmark, children, ...props}) =>
+const Checklist = ({ checkmark, children, ...props }) =>
   <Flex is='ul'
-    p={0} style={{listStyle: 'none'}}
+    p={0} style={{ listStyle: 'none' }}
     flexDirection='column'
     {...props}>
     {children.map((item, index) =>
@@ -33,7 +33,7 @@ Checklist.defaultProps = {
   children: []
 }
 Checklist.propTypes = {
-  checkmark: propTypes.string,
+  checkmark: propTypes.any,
   children: propTypes.any
 }
 export default Checklist
